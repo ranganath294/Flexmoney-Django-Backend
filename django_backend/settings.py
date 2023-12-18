e24793624af4b76e25888e41283b956e335cdae3
Ranganath <ranganathgottumukkala@gmail.com>
2023-12-18 18:56:13 +0530

    set

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ia5siw@0#z(zlqhlmcc1ogc68p6zqnr2b+u1@$lnw=j*p4y5d$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
